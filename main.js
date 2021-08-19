4e74bafc95c54a8e12b21fab28deb924f88198c7
MasterProatadvacedlevel12 <74993217+MasterProatadvacedlevel12@users.noreply.github.com>
2021-08-18 17:00:15 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,34 @@
+song = "";
+
 function setup(){
-    canvas = createCanvas(300,300);
-    canvas.position(540,130);
+    canvas = createCanvas(600,500);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+    }
+}
+
+function modelLoaded(){
+    console.log("Posenet is Loaded");
+}
+
+function draw(){
+    image(video,0,0,600,500);
+}
+
+
+function preload(){
+    song = loadSound("music2.mp3");
+}
+
+function playsong(){
+    song.play();
 }
