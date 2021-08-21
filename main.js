5b5edd1d16a86922142b791f11b9ca671c7ec591
MasterProatadvacedlevel12 <74993217+MasterProatadvacedlevel12@users.noreply.github.com>
2021-08-21 14:45:27 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 song = "";
-
+leftWristX=0;
+rightWristX=0;
+leftWristY=0;
+rightWristY=0;
 function setup(){
     canvas = createCanvas(600,500);
     canvas.center();
@@ -13,6 +16,14 @@ function setup(){
 function gotPoses(results){
     if(results.length > 0){
         console.log(results);
+        leftWristX= results[0].pose.leftWrist.x;
+        rightWristX= results[0].pose.rightWrist.x;
+        leftWristY= results[0].pose.leftWrist.y;
+        rightWristY= results[0].pose.rightWrist.y;
+        console.log("leftWristx:"+leftWristX);
+        console.log("leftWristx:"+leftWristX);
+        console.log("leftWristy:"+leftWristY);
+        console.log("rightWristy:"+rightWristY);
     }
 }
 
@@ -29,6 +40,6 @@ function preload(){
     song = loadSound("music2.mp3");
 }
 
-function playsong(){
+function songname(){
     song.play();
 }
