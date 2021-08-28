2d865d6bd77ed15faaf56043406be15c6c894aa4
MasterProatadvacedlevel12 <74993217+MasterProatadvacedlevel12@users.noreply.github.com>
2021-08-28 11:17:32 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
-song = "";
+song1 = "";
+song1status = "";
 leftWristX=0;
 rightWristX=0;
 leftWristY=0;
 rightWristY=0;
+scoreleftWrist=0;
+scorerightWrist=0;
+song2 = "";
+song2status = "";
+function preload(){
+    song1 = loadSound("music2.mp3");
+}
+
 function setup(){
     canvas = createCanvas(600,500);
     canvas.center();
@@ -15,14 +24,18 @@ function setup(){
 
 function gotPoses(results){
     if(results.length > 0){
+        scoreleftWrist = results[0].pose.keypoints[9].score;
+        scoreleftWrist = results[0].pose.keypoints[10].score;
+
+
         console.log(results);
         leftWristX= results[0].pose.leftWrist.x;
         rightWristX= results[0].pose.rightWrist.x;
         leftWristY= results[0].pose.leftWrist.y;
         rightWristY= results[0].pose.rightWrist.y;
         console.log("leftWristx:"+leftWristX);
-        console.log("leftWristx:"+leftWristX);
         console.log("leftWristy:"+leftWristY);
+        console.log("rightWristx:"+rightWristX);
         console.log("rightWristy:"+rightWristY);
     }
 }
@@ -33,13 +46,36 @@ function modelLoaded(){
 
 function draw(){
     image(video,0,0,600,500);
+    song1status = song1.isPlaying();
+    song2status = song2.isPlaying();     
+    fill("#FF0000");
+    stroke("#FF0000");
+
+    if(scoreleftWrist > 0.2){
+        circle(leftWristX,leftWristY,20);
+        song2.stop();
+    }
+
+    if(song1status = false){
+        song1.play();
+        document.getElementById("Songy").value
+    }
+
+
+
+    if(scorerightWrist > 0.2){
+        circle(rightWristX,rightWristY,20);
+        song1.stop();
+    }
+
+    if(song2status = false){
+        song2.play();
+        document.getElementById("Songy").value
+    }
+
+
+
 }
-
-
-function preload(){
-    song = loadSound("music2.mp3");
-}
-
 function songname(){
-    song.play();
+     song1.play();
 }
